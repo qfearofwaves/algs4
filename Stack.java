1ef0859c23e821e9e53f16c53c6f9c3461ed6ccb
qfearofwaves <qingniu.me@gmail.com>
2021-09-06 22:47:08 -0700

    Add Stack and Queue using LinkedList

---
+++ Stack.java
@@ -0,0 +1,49 @@
+public class Stack<Item> {
+    private Node first;     // top of stack (most recently added node)
+    private int n;          // number of items
+
+    private class Node {
+        Item item;
+        Node next;
+    }
+
+    public boolean isEmpty() {
+        return first == null;
+    }
+    public int size() {
+        return n;
+    }
+
+    public void push(Item item) {
+        Node oldfirst = first;
+        first = new Node();
+        first.item = item;
+        first.next = oldfirst;
+        n++;
+    }
+
+    public Item pop() {
+        Item item = first.item;
+        frist = first.next;
+        n--;
+        return item;
+    }
+
+    public Iterator<Item> iterator() {
+        return new ListIterator();
+    }
+
+    private class ListIterator implements Iterator<Item> {
+        private Node current = first;
+
+        public boolean hasNext() {
+            return current != null;
+        }
+        public void remove() {}
+        public Item next() {
+            Item item = current.item;
+            current = current.next;
+            return item;
+        }
+    }
+}
